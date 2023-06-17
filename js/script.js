d15b2f857ca4cc83f0f4abace77e54d3f5511afc
sobia001 <09799.exe@gmail.com>
2023-06-17 17:36:24 +0500

    page completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
- 
+$(document).ready(function () {
+    // Listen for checkbox change event
+    $('.form__check__input').change(function () {
+        // Check if the checkbox is checked
+        if ($(this).is(':checked')) {
+            // Change the background color of the parent div
+            $(this).closest('.form__check').css('background-color', '#9947E2');
+        } else {
+            // Revert the background color of the parent div to the default
+            $(this).closest('.form__check').css('background-color', '');
+        }
+    });
+}); 
